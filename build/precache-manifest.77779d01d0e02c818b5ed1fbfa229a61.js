064f0c554de74de3455a58e8478b14b89ffd9d1e
Atala <atala.rchidi@gmail.com>
2020-01-29 22:03:12 -0500

    deployed

---
+++ build/precache-manifest.77779d01d0e02c818b5ed1fbfa229a61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8419b66a2bd35666fb9febad17c6fb2c",
+    "url": "/My-contacts/index.html"
+  },
+  {
+    "revision": "06f2b282d206c8007888",
+    "url": "/My-contacts/static/js/2.7887fd67.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/My-contacts/static/js/2.7887fd67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8728fecd965f00639d03",
+    "url": "/My-contacts/static/js/main.9b2c45d7.chunk.js"
+  },
+  {
+    "revision": "831ab3eb75c58ad0cf42",
+    "url": "/My-contacts/static/js/runtime-main.86445b1b.js"
+  }
+]);
